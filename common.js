39464ae6e39ff35315a608cc4a3548aace61545b
Koh Zi Chun <zichun@gmail.com>
2012-07-12 10:06:00 +0800

    catch error thrown by node-phantom

--- common.js
+++ common.js
@@ -4,19 +4,23 @@ module.exports = {
 	openUrlWithJquery: function(url, cb) {
 		global.log.debug('Opening url ' + url);
 
-		phantom.create('--load-images=no','--local-to-remote-url-access=no', '--disk-cache=yes',function(ph) {
-			ph.createPage(function(page) {
-				return page.open(url, function (status) {
-					if (status !== 'success') {
-						cb('Error opening ' + url + ': ' + status, false);
-					} else {
-						page.injectJs('./jquery.js', function() {
-							cb(null, ph, page);
-						});
-					}
+		try {
+			phantom.create('--load-images=no','--local-to-remote-url-access=no', '--disk-cache=yes',function(ph) {
+				ph.createPage(function(page) {
+					return page.open(url, function (status) {
+						if (status !== 'success') {
+							cb(new Error('Error opening ' + url + ': ' + status), false);
+						} else {
+							page.injectJs('./jquery.js', function() {
+								cb(null, ph, page);
+							});
+						}
+					});
 				});
 			});
-		});
+		} catch (e) {
+			cb(e, null);
+		}
 
 	},
 
